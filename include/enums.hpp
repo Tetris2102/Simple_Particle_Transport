f3a50331d4d0688314d9325634cfaac62915eacc
Taras O. <tarantino0709@gmail.com>
2026-09-10 16:58:30 -0600

    Update enums.hpp

--- include/enums.hpp
+++ include/enums.hpp
@@ -13,16 +13,18 @@ struct PType
 {
     PName name;
     float mass;
+    short int charge;
 
-    PType(PName _name, float _mass) :
-        name(_name), mass(_mass) {}
+    PType(PName _name, float _mass, short int _charge) :
+        name(_name), mass(_mass), charge(_charge) {}
 };
 
+// Particle properties
 namespace Particles
 {
-    const PType ALPHA = PType{PName::ALPHA, 6.64e-27f};
-    const PType BETA = PType{PName::BETA, 9.11e-31f};
-    const PType GAMMA = PType{PName::GAMMA, 0.0f};
-    const PType NEUTRON = PType{PName::NEUTRON, 1.68e-27f};
-    const PType PROTON = PType{PName::PROTON, 1.67e-27f};
+    const PType ALPHA = PType{PName::ALPHA, 6.64e-27f, 2};
+    const PType BETA = PType{PName::BETA, 9.11e-31f, -1};
+    const PType GAMMA = PType{PName::GAMMA, 0.0f, 0};
+    const PType NEUTRON = PType{PName::NEUTRON, 1.68e-27f, 0};
+    const PType PROTON = PType{PName::PROTON, 1.67e-27f, 1};
 };
